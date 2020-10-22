6b5bcce8ebaa753b8dd277e703c012ebd35d2a4e
plxsas <sale12302006@yahoo.com>
2020-10-22 15:23:30 +0000

    added products detail page and sorting by categories

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404,redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product, Category
 
 
@@ -7,7 +9,35 @@ from .models import Product, Category
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, ("You didn't enter any search criteria!"))
+            return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
